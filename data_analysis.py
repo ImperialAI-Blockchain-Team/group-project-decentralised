c2cb169856e9c19b5f64a41c552ecd81aa533e6c
Marwan Mousa <marwan.mousa15@imperial.ac.uk>
2021-02-09 09:14:03 +0000

    Split data into 4 datasets

--- data_analysis.py
+++ data_analysis.py
@@ -8,6 +8,7 @@ Created on Mon Feb  8 21:10:41 2021
 import pandas as pd
 import numpy as np
 
+
 data_na = pd.read_csv('patient.csv')
 labels = ['patientunitstayid','patienthealthsystemstayid','wardid','hospitaladmittime24',
           'hospitaladmitoffset', 'hospitaldischargeyear','hospitaldischargetime24',
@@ -37,16 +38,25 @@ data.drop(['unittype'],axis=1, inplace=True)
 data = pd.concat([data,pd.get_dummies(data['unitadmitsource'], prefix='unitadmitsource')],axis=1)
 data.drop(['unitadmitsource'],axis=1, inplace=True)
 
+# split data into 4 different sets basedd on hospital idd
 data_1 = data.drop(data[data.hospitalid > 100].index)
 data_1 = data_1.drop(columns=['hospitalid'])
+data_1_labels = data_1['hospitaldischargestatus']
+data_1_inputs = data_1.drop(columns=['hospitaldischargestatus'])
 
 data_2 = data.drop(data[data.hospitalid <= 100].index)
 data_2 = data_2.drop(data_2[data_2.hospitalid > 200].index)
 data_2 = data_2.drop(columns=['hospitalid'])
+data_2_labels = data_2['hospitaldischargestatus']
+data_2_inputs = data_2.drop(columns=['hospitaldischargestatus'])
 
 data_3 = data.drop(data[data.hospitalid <= 200].index)
 data_3 = data_3.drop(data_3[data_3.hospitalid > 300].index)
 data_3 = data_3.drop(columns=['hospitalid'])
+data_3_labels = data_3['hospitaldischargestatus']
+data_3_inputs = data_3.drop(columns=['hospitaldischargestatus'])
 
 data_4 = data.drop(data[data.hospitalid <= 300].index)
 data_4 = data_4.drop(columns=['hospitalid'])
+data_4_labels = data_4['hospitaldischargestatus']
+data_4_inputs = data_4.drop(columns=['hospitaldischargestatus'])
