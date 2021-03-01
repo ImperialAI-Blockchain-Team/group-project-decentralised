6750497fecceb66991888d0263b7ee854173e2f5
Hu, Zepeng <zepeng.hu20@imperial.ac.uk>
2021-03-01 16:29:14 +0000

    Upload New File

---
+++ fl_network/server/server1.py
@@ -0,0 +1,283 @@
+"""Flower server example."""
+
+import argparse
+import flwr as fl
+from typing import Callable, Dict, List, Optional, Tuple
+
+from flwr.common import (
+    EvaluateIns,
+    EvaluateRes,
+    FitIns,
+    FitRes,
+    Weights,
+    parameters_to_weights,
+    weights_to_parameters,
+)
+from flwr.server.client_manager import ClientManager
+from flwr.server.client_proxy import ClientProxy
+
+from flwr.server.strategy.aggregate import aggregate, weighted_loss_avg
+
+import torch
+import torchvision
+import uploads.cifar
+import numpy as np
+DEFAULT_SERVER_ADDRESS = "[::]:8080"
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+DATA_ROOT = "~/data/patient.csv"
+
+class FedStrategy(fl.server.strategy.Strategy):
+    """Configurable FedAvg strategy implementation."""
+
+    # pylint: disable=too-many-arguments,too-many-instance-attributes
+    def __init__(
+        self,
+        fraction_fit: float = 0.1,
+        fraction_eval: float = 0.1,
+        min_fit_clients: int = 2,
+        min_eval_clients: int = 2,
+        min_available_clients: int = 2,
+        eval_fn = None,
+        on_fit_config_fn = None,
+        on_evaluate_config_fn = None,
+        accept_failures = True,
+    ) -> None:
+        """Federated Averaging strategy.
+        Args:
+            fraction_fit (float, optional): Fraction of clients used during
+                training. Defaults to 0.1.
+            fraction_eval (float, optional): Fraction of clients used during
+                validation. Defaults to 0.1.
+            min_fit_clients (int, optional): Minimum number of clients used
+                during training. Defaults to 2.
+            min_eval_clients (int, optional): Minimum number of clients used
+                during validation. Defaults to 2.
+            min_available_clients (int, optional): Minimum number of total
+                clients in the system. Defaults to 2.
+            eval_fn (Callable[[Weights], Optional[Tuple[float, float]]], optional):
+                Function used for validation. Defaults to None.
+            on_fit_config_fn (Callable[[int], Dict[str, Scalar]], optional):
+                Function used to configure training. Defaults to None.
+            on_evaluate_config_fn (Callable[[int], Dict[str, Scalar]], optional):
+                Function used to configure validation. Defaults to None.
+            accept_failures (bool, optional): Whether or not accept rounds
+                containing failures. Defaults to True.
+        """
+        super().__init__()
+        self.min_fit_clients = min_fit_clients
+        self.min_eval_clients = min_eval_clients
+        self.fraction_fit = fraction_fit
+        self.fraction_eval = fraction_eval
+        self.min_available_clients = min_available_clients
+        self.eval_fn = eval_fn
+        self.on_fit_config_fn = on_fit_config_fn
+        self.on_evaluate_config_fn = on_evaluate_config_fn
+        self.accept_failures = accept_failures
+
+    def __repr__(self) -> str:
+        rep = f"FedAvg(accept_failures={self.accept_failures})"
+        return rep
+
+    def num_fit_clients(self, num_available_clients: int):
+        """Return the sample size and the required number of available
+        clients."""
+        num_clients = int(num_available_clients * self.fraction_fit)
+        return max(num_clients, self.min_fit_clients), self.min_available_clients
+
+    def num_evaluation_clients(self, num_available_clients: int):
+        """Use a fraction of available clients for evaluation."""
+        num_clients = int(num_available_clients * self.fraction_eval)
+        return max(num_clients, self.min_eval_clients), self.min_available_clients
+
+
+    def evaluate(self, weights):
+        """Evaluate model weights using an evaluation function (if
+        provided)."""
+        if self.eval_fn is None:
+            # No evaluation function provided
+            return None
+        return self.eval_fn(weights)
+
+
+    def configure_fit(
+        self, rnd: int, weights, client_manager
+    ):
+        """Configure the next round of training."""
+        parameters = weights_to_parameters(weights)
+        config = {}
+        if self.on_fit_config_fn is not None:
+            # Custom fit config function provided
+            config = self.on_fit_config_fn(rnd)
+        fit_ins = FitIns(parameters, config)
+
+        # Sample clients
+        sample_size, min_num_clients = self.num_fit_clients(
+            client_manager.num_available()
+        )
+        clients = client_manager.sample(
+            num_clients=sample_size, min_num_clients=min_num_clients
+        )
+
+        # Return client/config pairs
+        return [(client, fit_ins) for client in clients]
+
+
+    def configure_evaluate(
+        self, rnd: int, weights: Weights, client_manager: ClientManager
+    ) -> List[Tuple[ClientProxy, EvaluateIns]]:
+        """Configure the next round of evaluation."""
+        # Do not configure federated evaluation if a centralized evaluation
+        # function is provided
+        if self.eval_fn is not None:
+            return []
+
+        # Parameters and config
+        parameters = weights_to_parameters(weights)
+        config = {}
+        if self.on_evaluate_config_fn is not None:
+            # Custom evaluation config function provided
+            config = self.on_evaluate_config_fn(rnd)
+        evaluate_ins = EvaluateIns(parameters, config)
+
+        # Sample clients
+        if rnd >= 0:
+            sample_size, min_num_clients = self.num_evaluation_clients(
+                client_manager.num_available()
+            )
+            clients = client_manager.sample(
+                num_clients=sample_size, min_num_clients=min_num_clients
+            )
+        else:
+            clients = list(client_manager.all().values())
+
+        # Return client/config pairs
+        return [(client, evaluate_ins) for client in clients]
+
+
+    def aggregate_fit(
+        self,
+        rnd: int,
+        results: List[Tuple[ClientProxy, FitRes]],
+        failures: List[BaseException],
+    ) -> Optional[Weights]:
+        """Aggregate fit results using weighted average."""
+        if not results:
+            return None
+        # Do not aggregate if there are failures and failures are not accepted
+        if not self.accept_failures and failures:
+            return None
+        # Convert results
+        weights_results = [
+            (parameters_to_weights(fit_res.parameters), fit_res.num_examples)
+            for client, fit_res in results
+        ]
+
+        weights = aggregate(weights_results)
+        if weights is not None:
+            # Save weights
+            print(f"Saving round {rnd} weights...")
+            np.savez(f"round-{rnd}-weights.npz", *weights)
+        return weights
+
+
+    def aggregate_evaluate(
+        self,
+        rnd: int,
+        results: List[Tuple[ClientProxy, EvaluateRes]],
+        failures: List[BaseException],
+    ) -> Optional[float]:
+        """Aggregate evaluation losses using weighted average."""
+        if not results:
+            return None
+        # Do not aggregate if there are failures and failures are not accepted
+        if not self.accept_failures and failures:
+            return None
+        return weighted_loss_avg(
+            [
+                (evaluate_res.num_examples, evaluate_res.loss, evaluate_res.accuracy)
+                for _, evaluate_res in results
+            ]
+        )
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Flower")
+    parser.add_argument(
+        "--server_address",
+        type=str,
+        default=DEFAULT_SERVER_ADDRESS,
+        help=f"gRPC server address (default: {DEFAULT_SERVER_ADDRESS})",
+    )
+    parser.add_argument(
+        "--rounds",
+        type=int,
+        default=1,
+        help="Number of rounds of federated learning (default: 1)",
+    )
+    parser.add_argument(
+        "--sample_fraction",
+        type=float,
+        default=1.0,
+        help="Fraction of available clients used for fit/evaluate (default: 1.0)",
+    )
+    parser.add_argument(
+        "--min_sample_size",
+        type=int,
+        default=2,
+        help="Minimum number of clients used for fit/evaluate (default: 2)",
+    )
+    parser.add_argument(
+        "--min_num_clients",
+        type=int,
+        default=2,
+        help="Minimum number of available clients required for sampling (default: 2)",
+    )
+    parser.add_argument(
+        "--log_host",
+        type=str,
+        help="Logserver address (no default)",
+    )
+    args = parser.parse_args()
+    def get_on_fit_config_fn() -> Callable[[int], Dict[str, str]]:
+        """Return a function which returns training configurations."""
+
+        def fit_config(rnd: int) -> Dict[str, str]:
+            """Return a configuration with static batch size and (local) epochs."""
+            config = {
+                "learning_rate": str(0.001),
+                "batch_size": str(32),
+		        "epochs": str(1),
+            }
+            return config
+
+        return fit_config
+
+
+    def get_eval_fn(
+        testset,
+    ) -> Callable[[fl.common.Weights], Optional[Tuple[float, float]]]:
+        """Return an evaluation function for centralized evaluation."""
+
+        def evaluate(weights: fl.common.Weights) -> Optional[Tuple[float, float]]:
+            """Use the entire CIFAR-10 test set for evaluation."""
+            model = cifar.Loader.load_model()
+            model.set_weights(weights)
+            model.to(DEVICE)
+            testloader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False)
+
+            return cifar.test(model, testloader, device=DEVICE)
+
+        return evaluate
+
+   # _, testloader = cifar.load_data(DATA_ROOT, batchsize=32)
+
+    strategy = FedStrategy(
+        fraction_fit=0.1,  # Sample 10% of available clients for the next round
+        min_fit_clients=2,  # Minimum number of clients to be sampled for the next round
+        min_available_clients=3,
+        on_fit_config_fn=get_on_fit_config_fn(),
+        #eval_fn=get_eval_fn(testloader)
+        # Minimum number of clients that need to be connected to the server before a training round can start
+    )
+    fl.server.start_server("0.0.0.0:8080", config={"num_rounds": 3}, strategy=strategy)
