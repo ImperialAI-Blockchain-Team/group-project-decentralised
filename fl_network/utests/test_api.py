9e1573b34a657c739b2f98a12932708ee4369dc2
Thomas <tfalezan@nukk.ai>
2021-03-04 01:00:49 +0000

    more api functional tests

--- fl_network/utests/test_api.py
+++ fl_network/utests/test_api.py
@@ -3,11 +3,36 @@ import requests
 import filecmp
 import os
 
-def test_upload_download():
-
+def test_upload():
     api_endpoint = 'http://127.0.0.1:5000/models'
 
-    # send file
+    # send test file
+    with open("test_file.txt","w") as f:
+        f.write('this is a test file\n')
+    with open("test_file.txt","r") as f:
+        file_dict = {'file': f}
+        r1 = requests.post(api_endpoint, files=file_dict)
+
+    # check operation success
+    assert (r1.json()['log'] == 'file uploaded')
+
+
+def test_download_inexistent_file():
+    api_endpoint = 'http://127.0.0.1:5000/models'
+
+    # retrieve inexistent file
+    params = {'file': 'inexistant_file.txt'}
+    r2 = requests.get(api_endpoint, params=params)
+
+    # check response
+    assert type(r2.json()) == dict
+    assert r2.json()['log'] == "file inexistant_file.txt not found"
+
+
+def test_upload_download():
+    api_endpoint = 'http://127.0.0.1:5000/models'
+
+    # send test file
     with open("test_file.txt","w") as f:
         f.write('this is a test file\n')
     with open("test_file.txt","r") as f:
@@ -26,3 +51,4 @@ def test_upload_download():
     # remove created files
     os.remove('test_file.txt')
     os.remove('test_file_returned.txt')
+
