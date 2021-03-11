368d918951fada666da618487e28477950b6604c
Thomas <tfalezan@nukk.ai>
2021-03-11 00:36:45 +0000

    register_interest method implemented + minor updates

--- fl_network/data_scientist/upload_model.py
+++ fl_network/data_scientist/upload_model.py
@@ -5,15 +5,26 @@ api_endpoint = 'http://127.0.0.1:5000/'
 FILE_NAME = 'model.py'
 
 if __name__ == '__main__':
-    # with open('models/test.txt', 'rb') as f:
-    #     print(f.read())
+
     # # Send files
     print('uploading files...')
     files = {'model': open('models/model.py','rb'), 'description': open('models/test.txt', 'rb')}
-    r = requests.post(api_endpoint+'models', files=files)
+    params = {'objective': 'predict need for ICU treatment based on lung CT scans'}
+    r = requests.post(api_endpoint+'models', files=files, params=params)
     print('response: ', r.json(), '\n')
 
     # # Get file list
-    print('retrieving model descriptions...')
-    r = requests.get(api_endpoint + 'model_descriptions')
+    print('retrieving model metadata...')
+    r = requests.get(api_endpoint + 'available_models')
+    print('response: ', r.json(), '\n')
+
+    # Register Interest in model
+    print('registering interest in model with index 1')
+    params = {'model_idx': 1}
+    r = requests.put(api_endpoint+'models', params=params)
+    print('response: ', r.json(), '\n')
+
+    # # Check if interest was registered
+    print('retrieving model metadata...')
+    r = requests.get(api_endpoint + 'available_models')
     print('response: ', r.json(), '\n')
