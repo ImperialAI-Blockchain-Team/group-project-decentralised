6dc9b7b9a72a6b96052d00aacc54a87c996829af
Thomas <tfalezan@nukk.ai>
2021-03-28 15:11:27 +0100

    contract updated

---
+++ contracts/assets.sol
@@ -0,0 +1,32 @@
+pragma solidity 0.8.3;
+
+contract FederatedLearningAssets {
+
+    struct Asset {
+        uint id;
+        address owner;
+        uint[] process;
+        uint[] download;
+    }
+
+    mapping(uint => Asset) public assets;
+    uint public asset_counter;
+
+    function register_asset(uint[] _process, uint[] _download) public {
+        asset_counter ++;
+        assets[asset_counter] = Asset({id: asset_counter, owner: msg.sender, process: _process, download: _download});
+    }
+
+    function modify_asset(uint _id, uint[] _process, uint[] _download) public {
+        require(assets[_id].owner == msg.sender);
+        assets[_id].process = _process;
+        assets[_id].download = _download;
+    }
+
+    function transfer_ownership(uint _id, address _new_owner) public {
+        require(assets[_id].owner == msg.sender);
+        assets[_id].owner = _new_owner;
+    }
+
+
+}
