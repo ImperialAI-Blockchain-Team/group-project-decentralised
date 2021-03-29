7e17c6d903a7c12f1444db2a89fcbe76817884cf
Thomas <tfalezan@nukk.ai>
2021-03-29 13:52:40 +0100

    review

--- fl_network/client_2/retrieve_model.py
+++ fl_network/client_2/retrieve_model.py
@@ -1,16 +1,16 @@
 import requests
 
 api_endpoint = 'http://127.0.0.1:5000/models'
-FILE_NAME = 'model.py'
 
 if __name__ == '__main__':
 
-    print('sending get request...')
-    params = {'file': FILE_NAME}
+    idx = 1
+    print('retrieving model...')
+    params = {'file_idx': idx}
     r = requests.get(api_endpoint, params=params)
-    # print('\n', r.headers)
+
     if r.headers.get('Content-Type') == 'application/json':
         print(r.json())
 
     else:
-        open('retrieved_models/'+FILE_NAME, 'wb').write(r.content)
+        open('retrieved_models/'+str(idx)+'.py', 'wb').write(r.content)
