e78b20547f74881f3ce4165ded10054ef923f84a
Marwan Mousa <marwan.mousa15@imperial.ac.uk>
2021-03-31 13:42:11 +0100

    Added condition to prevent registering existing asset

--- contracts/Database.sol
+++ contracts/Database.sol
@@ -7,19 +7,24 @@ contract AssetDatabase {
         string description;
         string objective;
         uint time;
+        bool registered;
     }
 
     mapping(string => Asset) public assets;
 
     // function to register an asset
     function register_asset(string memory _ipfsName, string memory _description, string memory _ipfsObjective) public {
-        assets[_ipfsName] = Asset({owner: msg.sender, description: _description, objective: _ipfsObjective, time: block.timestamp});
+        // Check if asset is already registered to prevent overwriting existing entry
+        if (assets[_ipfsName].registered) {
+            revert("This asset is already registered");
+        }
+        // Register new asset
+        assets[_ipfsName] = Asset({owner: msg.sender, description: _description, objective: _ipfsObjective, time: block.timestamp, registered: true});
     }
 
 
     // Function to view information of certain asset
-    function get_asset(string memory _ipfsName) public view returns (address, string memory, string memory, uint) {
-
+    function get_asset_meta(string memory _ipfsName) public view returns (address, string memory, string memory, uint) {
 
         return  (assets[_ipfsName].owner, assets[_ipfsName].description, assets[_ipfsName].objective, assets[_ipfsName].time);
     }
@@ -29,5 +34,5 @@ contract AssetDatabase {
         require(assets[_ipfsName].owner == msg.sender);
         delete(assets[_ipfsName]);
     }
-    
+
 }
