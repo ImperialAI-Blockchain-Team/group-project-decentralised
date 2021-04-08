d88ae672f2a25bbb1fce4aa66f3e977771367c0c
aab120 <anna.beer20@imperial.ac.uk>
2021-04-08 17:50:25 +0100

    Added Registration Contract

--- reactapp/BlockchainBackend/migrations/2_deploy_contracts.js
+++ reactapp/BlockchainBackend/migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 const ModelDatabase = artifacts.require("ModelDatabase.sol");
+const DatasetDatabase = artifacts.require("DatasetDatabase.sol");
+const Registry = artifacts.require("Registry.sol");
 
 module.exports = function(deployer) {
+ deployer.deploy(Registry)
  deployer.deploy(ModelDatabase);
+ deployer.deploy(DatasetDatabase);
 };
--- reactapp/src/datasetdatabase.js
+++ reactapp/src/datasetdatabase.js
@@ -1,6 +1,6 @@
 import web3 from './web3';
 import { datasetDatabase} from "./abi/abi";
 
-const address = '0x61d9Bf9c81F9A7C9c19a916ca575f6F7B84627b6';
+const address = '0xB35a93D015c254A7AA7B9C4A93C96Ac9d673ae2A';
 
 export default new web3.eth.Contract(datasetDatabase, address);
