f0c708bd612c8e8fafb558fe999ee455123e8a3f
Thomas <tfalezan@nukk.ai>
2021-04-08 23:19:12 +0100

    Data Browser implemented

--- reactapp/src/components/MenuBar/MenuBar.js
+++ reactapp/src/components/MenuBar/MenuBar.js
@@ -4,6 +4,7 @@ import {RegisterNodeForm} from "../FLNodes/RegisterForm.js";
 import {UploadModelForm} from "../Models/UploadForm.js";
 import {UploadDatasetForm} from "../Datasets/UploadForm.js";
 import {ModelBrowser} from "../Models/Browser.js";
+import {DataBrowser} from "../Datasets/Browser.js"
 
 export class MenuBar extends React.Component {
 
@@ -26,16 +27,18 @@ export class MenuBar extends React.Component {
             <div className="mainBody">
                 <div className="tab">
                     <button class="tablink" name="register" onClick={this.handleClick}>Register</button>
-                    <button class="tablink" name="models" onClick={this.handleClick}>Machine Learning Models</button>
+                    <button class="tablink" name="models" onClick={this.handleClick}>ML Models</button>
                     <button class="tablink" name="datasets" onClick={this.handleClick}>Datasets</button>
                     <button class="tablink" name="FLnodes" onClick={this.handleClick}>FL Aggregators</button>
                     <button class="tablink" name="search_model" onClick={this.handleClick}>Search Model</button>
+                    <button class="tablink" name="search_dataset" onClick={this.handleClick}>Search Dataset</button>
                 </div>
                 <div className="tabContent">
                     {(this.state.form === "models") ? <UploadModelForm />: null}
                     {(this.state.form === "datasets") ? <UploadDatasetForm /> : null}
                     {(this.state.form === "FLnodes") ? <RegisterNodeForm />: null}
                     {(this.state.form === "search_model") ? <ModelBrowser />: null}
+                    {(this.state.form === "search_dataset") ? <DataBrowser />: null}
                 </div>
             </div>
         )
--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />s
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
