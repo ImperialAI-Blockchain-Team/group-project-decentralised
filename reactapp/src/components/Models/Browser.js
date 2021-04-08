b7b9e0e5b7f5ccf7427bf97ac15854bbc8497479
Thomas <tfalezan@nukk.ai>
2021-04-08 14:34:30 +0100

    model browser completed

--- reactapp/src/components/Models/Browser.js
+++ reactapp/src/components/Models/Browser.js
@@ -12,35 +12,91 @@ export class ModelBrowser extends React.Component {
             searchValue: '',
             ethAddress: '',
             numberOfModels: -1,
-            modelList: null
-        }
+            modelHashList: [],
+            modelList: [],
+            renderedModelList: []
+            }
         this.handleOnKeyUp = this.handleOnKeyUp.bind(this);
 
-        // call smart contract to render model
-        this.getModelList();
+        // call smart contract to render models
+        this.getNumberOfModels()
+        .then(this.getModelList, (err) => {alert(err)})
+        .then(this.renderModels, (err) => {alert(err)});
     }
 
-    getModelList() {
-        // const ethAddress = await modeldatabase.options.address;
-        // this.setState({ethAddress});
-        const numberOfModels = modeldatabase.methods.getNumberOfModels.call();
-        this.setState({numberOfModels: numberOfModels})
+    getNumberOfModels = async () => {
+        let numberOfModels = await modeldatabase.methods.getNumberOfModels().call();
+        this.setState({numberOfModels: numberOfModels});
+        return new Promise((resolve, reject) => {
+            if (numberOfModels != -1) {
+                resolve(numberOfModels);
+            } else {
+                reject(Error("Can't connect to ModelDatabase smart contract."))
+            }
+        })
     }
 
-    handleOnKeyUp(event) {
+    getModelList = async (numberOfModels) => {
+        var newModelHashList = [];
+        var newModelList = [];
+        for (var i=0; i<numberOfModels; i++) {
+            const ipfsHash = await modeldatabase.methods.hashes(i).call();
+            const model = await modeldatabase.methods.models(ipfsHash).call();
+            newModelHashList.push(ipfsHash+ '---');
+            newModelList.push(model);
+        }
+        newModelHashList.reverse();
+        newModelList.reverse();
+        this.setState({modelHashList: newModelHashList})
+        this.setState({modelList: newModelList})
+
+        return new Promise((resolve, reject) => {
+            resolve(newModelList);
+        })
+    }
+
+
+    renderModels = async (modelList) => {
+        const subModelList = modelList.filter(model => {
+            return model['description'].toLowerCase().startsWith(this.state.searchValue)
+        })
+
+        const renderedModels = await subModelList.map(model => {
+            return (
+            <div className="modelContainer">
+                <div className="subModelContainer">
+                    <p><b>Owner</b>: {model['owner']}</p>
+                    <p><b>Description</b>: {model['description']}</p>
+                    <p><b>Creation Date</b>: {new Date(model['time']*1000).toLocaleDateString()}</p>
+                </div>
+            </div>
+            )
+        })
+        this.setState({renderedModelList: renderedModels});
+    }
+
+    handleOnKeyUp = async (event) => {
         const target = event.target;
         const name = target.name;
-        this.setState({searchValue: event.target.value});
+        await this.setState({searchValue: event.target.value});
+        this.renderModels(this.state.modelList);
     }
 
     render() {
+
         return (
             <div className="container">
                 <div className="searchBarContainer">
-                    <input type="text" id="myInput" onKeyUp={this.handleOnKeyUp} placeholder="Search model" />
+                    <input type="text" id="myInput" onKeyUp={this.handleOnKeyUp} placeholder="Search model (by description)" />
+                </div>
+                <div className="headerContainer">
+                    <p id="numberOfModels">{this.state.numberOfModels} models already uploaded to the system</p>
+                    <hr />
                 </div>
                 <div className="modelListContainer">
-                    <p>{this.state.numberOfModels}</p>
+                    <tr>
+                        <p>{this.state.renderedModelList}</p>
+                    </tr>
                 </div>
             </div>
         )
