a42cd8d704f17a5195271ec7bb611af220602aee
ra2820 <roopa.antony20@imperial.ac.uk>
2021-04-17 21:48:22 +0100

    added enzyme adapter

--- reactapp/src/components/forms/user.test.js
+++ reactapp/src/components/forms/user.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { getByText, render,screen } from '@testing-library/react';
 
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
- 
+//import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 Enzyme.configure({ adapter: new Adapter() });
 
 import {shallow} from 'enzyme'
